10387fde4e7584fb1b55191bacb259f596ef837a
Mikhail <derkach94@gmail.com>
2018-11-22 15:35:06 +0300

    remove script, height set to vh by css

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,38 @@
-document.addEventListener('DOMContentLoaded', function(){
-  // set login page content to viewport height
-  var header = document.querySelector('header').clientHeight;
-  var container = document.querySelector('.main-info__wrapper');
-  var container1 = document.querySelector('.side-info__wrapper');
-  var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  function appendHeight() {
-    if (window.innerWidth > 991) {
-      container.style.height = viewportHeight - header + "px";
-      container1.style.height = viewportHeight - header + "px";
-    } else {
-      container.style.height = "auto";
-      container1.style.height = "auto";
-    }
-  }
-  window.addEventListener("resize", function () {
-    appendHeight();
-  });
-  appendHeight();
-  
+document.addEventListener('DOMContentLoaded', function () {
+  // // set login page content to viewport height
+  // var header = document.querySelector('header').clientHeight;
+  // var container = document.querySelector('.main-info__wrapper');
+  // var container1 = document.querySelector('.side-info__wrapper');
+  // function getVH() {
+  //   var viewPortHeight;
+  //   // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+  //   if (typeof window.innerWidth != 'undefined') {
+  //     viewPortHeight = window.innerHeight
+  //   }
+  //   // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+  //   else if (typeof document.documentElement != 'undefined'
+  //     && typeof document.documentElement.clientWidth !=
+  //     'undefined' && document.documentElement.clientWidth != 0) {
+  //     viewPortHeight = document.documentElement.clientHeight
+  //   }
+  //   // older versions of IE
+  //   else {
+  //     viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+  //   }
+  //   return viewPortHeight;
+  // }
+  // function appendHeight() {
+  //   if (window.innerWidth > 991) {
+  //     container.style.height = getVH() - header + "px";
+  //     container1.style.height = getVH() - header + "px";
+  //   } else {
+  //     container.style.height = "auto";
+  //     container1.style.height = "auto";
+  //   }
+  // }
+  // window.addEventListener("resize", function () {
+  //   appendHeight();
+  // });
+  // appendHeight();
+
 });
