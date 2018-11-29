01fd0d6502b9777eaddff1546fdd6d4482889c3a
Skeev_ <derkach94@gmail.com>
2018-11-29 18:50:56 +0300

    rework v2

--- app/js/common.js
+++ app/js/common.js
@@ -7,42 +7,58 @@ document.addEventListener('DOMContentLoaded', function () {
     var viewPortHeight;
     // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
     if (typeof window.innerWidth != 'undefined') {
-      viewPortHeight = window.innerHeight
+      viewPortHeight = window.innerHeight;
     }
     // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-    else if (typeof document.documentElement != 'undefined'
-      && typeof document.documentElement.clientWidth !=
-      'undefined' && document.documentElement.clientWidth != 0) {
-      viewPortHeight = document.documentElement.clientHeight
+    else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+      viewPortHeight = document.documentElement.clientHeight;
     }
     // older versions of IE
     else {
-      viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+      viewPortHeight = document.getElementsByTagName('body')[0].clientHeight;
     }
     return viewPortHeight;
   }
   function appendHeight() {
     if (window.innerWidth > 991) {
-      container.style.height = getVH() - header + "px";
-      container1.style.height = getVH() - header + "px";
+      container.style.height = getVH() - header + 'px';
+      container1.style.height = getVH() - header + 'px';
     } else {
-      container.style.height = "auto";
-      container1.style.height = "auto";
+      container.style.height = 'auto';
+      container1.style.height = 'auto';
     }
   }
-  window.addEventListener("resize", function () {
+  window.addEventListener('resize', function () {
     appendHeight();
   });
   appendHeight();
-
+  // login page error show
   var form = document.querySelector('.form__header');
   var login = form.querySelector('input[type=text]').checkValidity();
   var password = form.querySelector('input[type=password]').checkValidity();
   var errorMessage = document.querySelector('.invalid');
- 
-  if (login == false || password == false){
+
+  if (login == false || password == false) {
     errorMessage.style.visibility = 'visible';
   } else {
     errorMessage.style.visibility = 'hidden';
-  }  
+  }
+});
+$(function () {
+  //video custom button
+  var playButton = $('.btn__play');
+  // Event listener for the play/pause button
+  playButton.on('click', function () {
+    var parentVideo = $(this).parent();
+    var childVideo = parentVideo.children('video');
+
+    if (childVideo.play == true) {
+      childVideo.trigger('pause');
+      $(this).css('visibility', 'visible');
+    } else {
+      childVideo.trigger('play');
+      $(this).css('visibility', 'hidden');
+    }
+  });
+
 });
